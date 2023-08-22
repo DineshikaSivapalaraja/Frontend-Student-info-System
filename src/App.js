688994138666a040c7268c3d0c7f573bf8226f6f
Dineshika <91914577+DineshikaSivapalaraja@users.noreply.github.com>
2023-08-22 23:55:32 +0530

    Create app using React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Student from './components/Student'
+
+function App() {
+  return (
+    <div className="App">
+        {/* Hey This is Dineshika! */}
+        <Student/>
+    </div>
+  );
+}
+
+export default App;
