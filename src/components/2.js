1f81848c8a2f565f32b2f0b58e92bf62be701ad3
Dineshika <91914577+DineshikaSivapalaraja@users.noreply.github.com>
2023-09-23 23:17:08 +0530

    Delete src/components/2.js

--- src/components/2.js
+++
@@ -1,9 +0,0 @@
-// export default function app2(){
-//     hello
-// }
-
-export default function getContent() {
-    return "hello";
-}
-
-{/* <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Have an account? Login here.</button> */}
